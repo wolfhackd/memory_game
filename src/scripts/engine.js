b9c7deba7a46bce95256e1955c0a10d5500d5ebb
Mauro Leal <137218372+wolfhackd@users.noreply.github.com>
2024-01-22 14:42:03 -0300

    complete

---
+++ src/scripts/engine.js
@@ -0,0 +1,59 @@
+const emojis = [
+    "🐱‍👤",
+    "🐱‍👤",
+    "🐱‍🏍",
+    "🐱‍🏍",
+    "🐱‍👓",
+    "🐱‍👓",
+    "🐱‍🐉",
+    "🐱‍🐉",
+    "🐱‍🚀",
+    "🐱‍🚀",
+    "🥽",
+    "🥽",
+    "🧦",
+    "🧦",
+    "💎",
+    "💎"
+];
+let openCards = [];
+
+//random
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("VocÊ venceu !");
+        window.document.location.reload();
+    }
+}
